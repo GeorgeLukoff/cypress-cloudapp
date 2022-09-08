83c31eb5159c921983a4fec250ee1bc31eb8b4eb
GeorgeLukoff <57776497+GeorgeLukoff@users.noreply.github.com>
2022-09-08 13:03:38 -0700

    Update user-signup.js

--- user-signup.js
+++ user-signup.js
@@ -1,29 +1,18 @@
 import { pageObject as po } from "./page-object"
-import { cloudAppUser } from "../../fixtures/secrets"
 
 export function userSignup(username, password) {
-
-    let review_recaptcha_widget
-    let onloadCallback = function () {
-        if ($('#review_recaptcha').length) {
-            review_recaptcha_widget = grecaptcha.render('review_recaptcha', {
-                'sitekey': '<?php echo $site_key?>'
-            })
-        }
-    }
-
-    // const user = cloudAppUser
 
     const baseUrl = "https://share.getcloudapp.com"
 
     cy.visit(`${baseUrl}/signup`)
 
     const emailInputElement =
-        cy.get(po.globalSelectors.emailInput).type(username)
+        cy.get(po.emailInput)
+    emailInputElement.type(username)
 
     const passwordInputElement =
-        cy.get(po.globalSelectors.passwordInput)
+        cy.get(po.passwordInput)
     passwordInputElement.type(password)
 
-    cy.get(po.globalSelectors.submitButton).click()
+    cy.get(po.submitButton).click()
 }
