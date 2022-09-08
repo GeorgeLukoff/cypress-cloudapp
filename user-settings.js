8ed300728b86ed036b51acae3765f4d656546500
GeorgeLukoff <57776497+GeorgeLukoff@users.noreply.github.com>
2022-09-08 13:01:52 -0700

    Update user-settings.js

--- user-settings.js
+++ user-settings.js
@@ -1,14 +1,17 @@
 import { pageObject as po } from "./page-object"
-import { cloudAppUser } from "../../fixtures/secrets"
+
 
 export function userSettingUpdate() {
+
+    const fileName = 'hamster.jpeg'
+
     cy.get('#avatar').click()
     cy.get('.dropdown-item').contains('Settings').click()
-    cy.get(fileInput).attachFile(fileName)
-    cy.get(userFullName).type(fullName).clear()
-    cy.get(userFullName).type(fullName)
-    cy.get(userCompany).type(role).clear()
-    cy.get(userCompany).type(role)
-    cy.get(userRole).select(7).should('have.value', 'information_technology')
-    cy.get(submitButton).contains('Submit').should('be.visible').click()
+    cy.get(po.selectors.inputFile).attachFile(fileName)
+    cy.get(po.selectors.userFullName).type(po.profile.fullName).clear()
+    cy.get(po.selectors.userFullName).type(po.profile.fullName)
+    cy.get(po.selectors.userCompany).type(po.profile.role).clear()
+    cy.get(po.selectors.userCompany).type(po.profile.role)
+    cy.get(po.selectors.userRole).select(7).should('have.value', 'information_technology')
+    cy.get(po.selectors.submitButton).contains('Submit').should('be.visible').click()
 }
