c382eca3738c6568b3e17569883046416d3e3fa6
GeorgeLukoff <57776497+GeorgeLukoff@users.noreply.github.com>
2022-09-08 12:59:53 -0700

    Update page-object.js

--- page-object.js
+++ page-object.js
@@ -1,11 +1,27 @@
+import { userLogin } from "./user-login"
+import { userSignup } from "./user-signup"
+import { userSettingUpdate } from "./user-settings"
+
+
 export const pageObject = {
-    globalSelectors: {
+    userLogin: userLogin,
+    userSignup: userSignup,
+    userSettingUpdate: userSettingUpdate,
+    // verifyAvatar: verifyAvatar,
+
+    selectors: {
         emailInput: 'input[type="email"]',
         passwordInput: 'input[type="password"]',
         submitButton: 'input[type="submit"]',
         userFullName: 'input[placeholder = "Name"]',
         userCompany: 'input[placeholder = "ex: Apple Inc."]',
         userRole: 'select[name = "user[profile]"]',
-        fileInput: 'input[type="file"]'
+        inputFile: 'input[type="file"]'
+    },
+
+    profile: {
+        fullName: "George Lukoff",
+        company: "Self",
+        role: "Information Technology"
     }
 }
